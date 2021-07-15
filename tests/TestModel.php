3d44d576916b09859a28b80e9a4f87f495d9386d
Sergey Shiryaev <s.shiryaev@qsoft.ru>
2021-07-15 11:55:00 +0300

    remove HasFactory trait this trait appeared only in laravel 8

--- tests/TestModel.php
+++ tests/TestModel.php
@@ -2,13 +2,11 @@
 
 namespace SShiryaev\LaravelTranslatable\Tests;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use SShiryaev\LaravelTranslatable\Translatable;
 
 class TestModel extends Model
 {
-    use HasFactory;
     use Translatable;
 
     protected $table = 'test_models';
